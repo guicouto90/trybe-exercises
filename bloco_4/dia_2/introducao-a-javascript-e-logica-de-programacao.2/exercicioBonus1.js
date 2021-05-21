a4e9c3f472157c5df603d1a31724d24688b7d6cf
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-05-21 14:55:22 -0300

    Exercicio bonus do dia 2 de programaçao

--- bloco_4/dia_2/introducao-a-javascript-e-logica-de-programacao.2/exercicioBonus1.js
+++ bloco_4/dia_2/introducao-a-javascript-e-logica-de-programacao.2/exercicioBonus1.js
@@ -1,20 +1,44 @@
-let array = new Array(25);
-console.log(array.length);
+/*let array = new Array(5);
+//console.log(array.length);
 
-for(let contador = 1; contador < array.length; contador +=1){
-    array[contador] = Math.ceil(Math.random()*100);
+for(let contador = 0; contador < array.length; contador +=1){
+    array[contador] = Math.ceil(Math.random()*10);
     console.log(array[contador]);
 }
 
 
 
-for (let index = 1; index < array.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (array[index] < array[secondIndex]) {
-        let position = array[index];
-        array[index] = array[secondIndex];
-        array[secondIndex] = position;
-      }
-      console.log(array[index]);
+console.log("-----------------------------------------------")
+
+for(let contador1 = 1; contador1 < array.length; contador1 +=1){
+  for(let contador2 = 0; contador2< contador1; contador2=+1){
+    if(array[contador1]< array[contador2]){
+      let posicao = array[contador1];
+      array[contador1] = array[contador2];
+      array[contador2] = posicao;  
     }
+      
   }
+}
+console.log("-----------------------------------------------")
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);*/
+
+let array = [9,4,6,8,10];
+
+for(let contador1 = 1; contador1 < array.length; contador1 +=1){
+  for(let contador2 = 0; contador2< contador1; contador2=+1){
+    if(array[contador1]< array[contador2]){
+      let posicao = array[contador1];
+      array[contador1] = array[contador2];
+      array[contador2] = posicao;  
+    }
+      
+  }
+}
+
+console.log(array);
