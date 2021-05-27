4dac67e68098c448b5144e356e54bfaf3afd1aca
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-05-26 21:14:58 -0300

    Exercicio do bloco 5 dia 1

---
+++ bloco_5/dia_1/exercicios/script.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("p")[0].innerText = "Em 2 anos estarei com 33, e quero ter tido 1 ano de experiencia na area de tecnologia, quero estar morando sozinho, e ter pelo menos 3 fontes de rendas diferentes.";
+document.querySelector(".main-content").style.backgroundColor = "rgb(76,164,109)";
+document.querySelector(".center-content").style.backgroundColor = "white";
+document.querySelector(".title").innerText = "Exercício 5.1 - JavaScript";
+document.querySelector(".center-content").
